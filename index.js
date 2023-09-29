773b6bfd2c0b711514befb8cb702424373b8c57a
Eduardo Tejada <et3858@gmail.com>
2023-09-29 15:57:18 -0300

    Endpoints: first approach

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const app = express();
+const { Video } = require("./models");
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World! LOL');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.get('/', async (req, res) => {
+    const videos = await Video.findAll();
+
+    res.status(200).json({ data: videos });
+});
+
+app.post('/', async (req, res) => {
+    res.status(201).json({
+        body: req.body,
+    });
+});
+
+app.get('/:id', async (req, res) => {
+    const video = await Video.findByPk(req.params.id);
+
+    if (!video) {
+        res.status(404).json({ error: { msg: "Video not found" } });
+    } else {
+        res.status(200).json({ data: video });
+    }
+});
+
+app.delete('/:id', async (req, res) => {
+    const video = await Video.findByPk(req.params.id);
+
+    if (!video) {
+        res.status(404).json({ error: { msg: "Video not found" } });
+    } else {
+        res.status(200).json({ data: video });
+    }
 });
 
 app.listen(port, () => {
